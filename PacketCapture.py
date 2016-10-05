ad72fc070c37abf26aacc23e49d612b664f07296
Frank Mock <FMock@users.noreply.github.com>
2016-10-05 15:01:42 -0700

    Added entropy.py which is the main GUI and PacketCapture.py which the GUI uses

---
+++ PacketCapture.py
@@ -0,0 +1,47 @@
+import socket
+import os
+
+# A Class to capture network packets
+# Captured raw packets are stored as strings in the self.packets list
+# Much of the packet capture code is from the book Black Hat Python by 
+# Justin Seitz. We wrapped the code in a class to be used by our EntropytGUI
+# class.
+
+class PacketCapture:
+    def __init__(self):
+        self.packets = []
+        self.ipAddr = ""
+        
+    def capturePackets(self, ipAddr):
+        host = ipAddr
+
+        # create a raw socket and bind it to the public interface
+        if os.name == "nt":
+            socket_protocol = socket.IPPROTO_IP
+        else:
+            socket_protocol = socket.IPPROTO_ICMP
+
+        sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+        sniffer.bind((host, 0))
+
+        # we want the IP headers included in the capture
+        sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+        # if we're on Windows we need to send an IOCTL
+        # to setup promiscuous mode
+        if os.name == "nt":
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+        # read in a single packet
+        rawPacket = sniffer.recvfrom(65565)
+        
+        # add packet to list
+        self.packets.append(str(rawPacket))
+
+        # if we're on Windows turn off promiscuous mode
+        if os.name == "nt":
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            
+    def getPacketList(self):
+        return self.packets
---
+++ entropy.py
@@ -0,0 +1,66 @@
+from Tkinter import *
+from PacketCapture import PacketCapture
+
+# Packet Capture Program
+# This GUI uses our PacketCapture class
+
+
+class EntropyGUI:
+    def __init__(self):
+        
+        #self.pc = PacketCapture()
+
+        root = Tk()  # Create Window
+
+        root.title("Packet Capture")
+
+        Label(root, text="This program will capture a single packet and display the raw data below").pack()
+
+        Label(root, text="There needs to be network traffic so open a command prompt and execute a ping").pack()
+
+        Label(root, text="Note that this program needs to be run with administrator privileges").pack()
+        
+        Label(root, text="").pack()  # Spacer 
+        Label(root, text="Enter Host IP").pack()
+        
+        # Entrybox to get the host IP
+        self.ipAddr = StringVar()
+        self.textEntry = Entry(root, textvariable = self.ipAddr)
+        self.textEntry.pack()
+        
+        # Button to trigger the packet capture
+        button = Button(root)
+        button.configure(text="Capture Packet")
+        button.bind("<Button-1>", self.buttonClickCallback)
+        button.pack()
+        Label(root, text="").pack()  # Spacer        
+
+        # include a Scrollbar to connect to the Text where all the packets get printed
+        scrollbar = Scrollbar(root)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.textArea = Text(root)
+        self.textArea.pack()
+
+        scrollbar.config(command=self.textArea.yview)
+
+        root.mainloop() # main GUI  loop
+
+    #Callback method activated when the left mouse key clicks the  "Capture
+    # Packet" button
+    def buttonClickCallback(self, event):
+        host = self.ipAddr.get()
+        
+        # Make sure an IP address was entered
+        # This validity check must be improved
+        if host == "":
+            self.textArea.insert(INSERT, "You must enter a valid IP address\n")
+            return
+        else:
+            pc = PacketCapture()
+            pc.capturePackets(host)
+            myList = pc.getPacketList()
+            for p in myList:
+                self.textArea.insert(END, p)        
+
+EntropyGUI()  # Create the GUI
