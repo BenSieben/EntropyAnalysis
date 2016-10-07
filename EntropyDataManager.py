732d3b31e21ddf8f0cceafe35d51cdd53b8c401a
Ben <waripod@hotmail.com>
2016-10-07 15:01:15 -0700

    Added open / save options in a File drop down one the GUI, which in turn let the user open and save text files to and from the text area on the GUI

--- EntropyDataManager.py
+++ EntropyDataManager.py
@@ -23,24 +23,24 @@ class EntropyDataManager:
         json.dump(data, f)
 
 # testing the class
-e = EntropyDataManager("test.txt")
-data = [["1", 3], ["a", "b", "cccc", 50]]
-print(data)
-e.saveFile(data)
-savedData = e.openFile()
-print(savedData)
-print(savedData[1][1])
-print(type(savedData[1][1]))
-if data[1][1] == savedData[1][1]:
-    print("saved string matches original string")
-
-# testing if this still works on Packet class
-edm = EntropyDataManager("test2.txt")
-packetData = []
-for i in range(0, 10):
-    p = Packet("Site " + str(i), "Head " + str(i), "Body " + str(i), i)
-    packetData.append(p)
-print(packetData[2])
+# e = EntropyDataManager("test.txt")
+# data = [["1", 3], ["a", "b", "cccc", 50]]
+# print(data)
+# e.saveFile(data)
+# savedData = e.openFile()
+# print(savedData)
+# print(savedData[1][1])
+# print(type(savedData[1][1]))
+# if data[1][1] == savedData[1][1]:
+#     print("saved string matches original string")
+#
+# # testing if this still works on Packet class
+# edm = EntropyDataManager("test2.txt")
+# packetData = []
+# for i in range(0, 10):
+#     p = Packet("Site " + str(i), "Head " + str(i), "Body " + str(i), i)
+#     packetData.append(p)
+# print(packetData[2])
 # this will FAIL; Packet objects are not considered JSON serializable
 # edm.saveFile(packetData)
 # for p in packetData:
