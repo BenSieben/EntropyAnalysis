285cb9ba8d5e977f5a497ec5d7431ef96f1e8345
FMock <FMock@users.noreply.github.com>
2016-10-07 10:14:52 -0700

    GUI and PacketCapture Updated

--- PacketCapture.py
+++ PacketCapture.py
@@ -1,19 +1,21 @@
 import socket
 import os
+import struct
 
 # A Class to capture network packets
 # Captured raw packets are stored as strings in the self.packets list
-# Much of the packet capture code is from the book Black Hat Python by 
-# Justin Seitz. We wrapped the code in a class to be used by our EntropytGUI
-# class.
 
 class PacketCapture:
     def __init__(self):
-        self.packets = []
+        self.packets = [""]
         self.ipAddr = ""
-        
-    def capturePackets(self, ipAddr):
+        self.numPackets = 0
+
+    #Capture Packets and add to list    
+    def capturePackets(self, ipAddr, numPackets):
+        self.packets = [] # clear out the list
         self.ipAddr = ipAddr
+        self.numPackets = numPackets
 
         # create a raw socket and bind it to the public interface
         if os.name == "nt":
@@ -26,22 +28,24 @@ class PacketCapture:
         sniffer.bind((self.ipAddr, 0))
 
         # we want the IP headers included in the capture
-        sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        #sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
         # if we're on Windows we need to send an IOCTL
         # to setup promiscuous mode
         if os.name == "nt":
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-        # read in a single packet
-        rawPacket = sniffer.recvfrom(65565)
-        
-        # add packet to list
-        self.packets.append(str(rawPacket))
+            
+        # read in a specified number of ethernet frames
+        for i in range(self.numPackets):
+            # Recieve packet
+            rawPacket = sniffer.recvfrom(65565)
+                        
+            # add data part to list - data starts at byte 14 of ethernet frame
+            self.packets.append(str(rawPacket[:14]) + "\n") #TODO - remove "\n"
 
         # if we're on Windows turn off promiscuous mode
         if os.name == "nt":
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-            
+                        
     def getPacketList(self):
         return self.packets
