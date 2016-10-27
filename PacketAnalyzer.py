08ab9aa7c604fedecc0c277cabf64df66a5d13a9
Ben <waripod@hotmail.com>
2016-10-27 08:41:28 -0700

    Fixed up some naming in PacketAnalyzer

--- PacketAnalyzer.py
+++ PacketAnalyzer.py
@@ -12,14 +12,14 @@ class PacketAnalyzer:
     # Converts a list of raw packet captures into a filtered
     # list of packet captures.
     # rawPackets is a list of raw packet captures
-    # Returns the filitered form of the list
+    # Returns the filtered form of the list
     def filterPackets(self, rawPackets):
         print"Analyzing packets"
     
     # Converts a list of filtered packet data into binary
     # filteredPackets is a list of filtered packet data
     # Returns the binary form of the list
-    def convertToBinary(self, filterdPackets):
+    def convertToBinary(self, filteredPackets):
         pass
     
     # Analyzes the binary form of the packet data using
